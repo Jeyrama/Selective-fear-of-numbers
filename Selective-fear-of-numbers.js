860106c27c1d308ac11689f3dd92c494f2516fd3
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-28 10:06:12 -0700

    Update Selective-fear-of-numbers.js

--- Selective-fear-of-numbers.js
+++ Selective-fear-of-numbers.js
@@ -14,3 +14,6 @@ Sunday --> 666 or -666
 Write a function which takes a string (day of the week) and an integer 
 (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
 */
+
+
+//Solution
