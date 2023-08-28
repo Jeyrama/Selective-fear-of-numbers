9b9feede421f857676e1da3a9bbfdb55df3535d5
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-28 10:16:43 -0700

    Update Selective-fear-of-numbers.js

--- Selective-fear-of-numbers.js
+++ Selective-fear-of-numbers.js
@@ -17,3 +17,15 @@ Write a function which takes a string (day of the week) and an integer
 
 
 //Solution
+
+function AmIAfraid(day, num) {
+  return {
+    "Monday"    : num === 12,
+    "Tuesday"   : num > 95,
+    "Wednesday" : num === 34,
+    "Thursday"  : num === 0,
+    "Friday"    : num % 2 === 0,
+    "Saturday"  : num === 56,
+    "Sunday"    : Math.abs(num) === 666
+  }[day];
+}
